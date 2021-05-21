5de657bebf9523a724aa3c0749faf13634629b48
Julimar Mees <>
2021-05-21 21:31:11 +0000

    Fiz a lógica para identificar o maior e o menor numero

--- main.cpp
+++ main.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 void  min_max(int inum, int *pnums[], int *pmin, int *pmax){
 
+  for (int i=0; i<inum; i++){
+    
+    if (*pnums[i] > *pmax){
+      *pmax = *pnums[i];
+    }
+    
+    if (*pnums[i] < *pmin){
+      *pmin = *pnums[i];
+    }
+  }
+  
 }
 
 int main() {
-  int inum=0, *pmin, *pmax=0;
+  int inum=0, min=0, max=0;
 
   cout << "Informe a quantidade de numeros de entrada: \n ";
   cin >> inum;
@@ -17,5 +28,7 @@ int main() {
     cin >> nums[i];
     pnums[i] = &nums[i];
   }
-min_max(inum, pnums, pmin, pmax);
+min_max(inum, pnums, &min, &max);
+cout << min<<endl;
+cout << max<< endl;
 }
