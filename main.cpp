e0fb610c1698fdcb7e1e658c97f348a6f82247d2
Julimar Mees <>
2021-05-21 20:53:52 +0000

    criei a função

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
+
+void  min_max(int inum, int *pnums[], int *pmin, int *pmax){
+
+}
+
 int main() {
-  int inum=0;
+  int inum=0, *pmin, *pmax=0;
 
   cout << "Informe a quantidade de numeros de entrada: \n ";
   cin >> inum;
-  int nums[inum], *ptnum[inum];
+  int nums[inum], *pnums[inum];
 
   for (int i=0; i<inum; i++){
     cout << "digite o "<<i+1 << "º numero: \n";
     cin >> nums[i];
-    ptnum[i] = &nums[i];
+    pnums[i] = &nums[i];
   }
-
+min_max(inum, pnums, pmin, pmax);
 }
